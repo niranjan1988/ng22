df24669fa693224c0f14377464999b8ddcdf6211
Niranjan <nvallivedu@dalet.com>
2026-06-28 22:48:00 +0530

    Signal, computed, untracked, equal check

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, WritableSignal, computed,untracked, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,33 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('ng22');
+  protected readonly title: WritableSignal<string> = signal('ng22', {
+    equal: (a, b) => a.toLowerCase() === b.toLowerCase()
+  });
+  protected readonly description: WritableSignal<string> = signal('description');
+  protected readonly subtitle = computed(()=> "Learn " + this.title());
+  protected readonly titleText = computed(()=> this.title() + " - " + this.subtitle() + " - " + this.description());
+  protected readonly untrackedDescription = computed(()=> this.title() + " - " + this.subtitle() + " - " + untracked(this.description));
+
+
+  protected readonly pageItems=['Home','About','Contact','Services','Products','Blog','Careers','Support','FAQ','Testimonials'];
+
+  constructor() {
+    effect(() => {
+      console.log("Title changed to: " + this.title());
+    });
+  }
+
+  setTitle(newTitle: string) {
+    this.title.set(newTitle);
+  }
+
+    setDescription(newDescription: string) {
+    this.description.set(newDescription);
+  }
+
+  updateTitle() {
+    this.title.update((t) => "New " + t);
+  }
+
 }
